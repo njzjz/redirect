e04633dd9fd92b18b8ae0156af4ae622ffc6a405
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-06-14 04:50:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use('/', function (req, res) {
 	var host = req.get('host');
 	var url = req.originalUrl;
 	var redirect = domain[host];
-	if (!redirect) redirect = domain['*'];
+	if (!redirect || url=="/robots.txt") redirect = domain['*'];
 	res.redirect(301, redirect+url);
 })
 module.exports = app;
