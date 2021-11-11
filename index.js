52908cc5640840a0dc24f8ed5fa5d5449143e086
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-11-11 03:47:15 -0500

    set cache

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var domain = require('./domain');
 var app = express();
 app.use('/', function (req, res) {
+	res.setHeader('Cache-Control', 's-max-age=3600000, stale-while-revalidate');
 	var host = req.get('host');
 	var url = req.originalUrl;
 	var redirect = domain[host];
