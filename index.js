fd6347bc3925f4c1fec9e18eb5c52e747525a52b
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-04-22 22:24:46 -0400

    redirect conda to doc if user-agent is incorrect

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-var express = require('express');
-var domain = require('./domain');
-var app = express();
+const express = require('express');
+const domain = require('./domain');
+const app = express();
 app.use('/', function (req, res) {
 	res.setHeader('Cache-Control', 's-max-age=3600000, stale-while-revalidate');
-	var host = req.get('host');
-	var url = req.originalUrl;
+	const host = req.get('host');
+	const url = req.originalUrl;
 	var redirect = domain[host];
 	if (!redirect || url=="/robots.txt") redirect = domain['*'];
+	if (host == 'conda.deepmodeling.org') {
+		// redirect to doc if not conda or mamba
+		const agent = req.get('User-Agent');
+		if (!'conda' in agent && !'mamba' in agent) redirect = "https://docs.deepmodeling.com/projects/deepmd/en/latest/install/easy-install.html#install-with-conda";
+	}
 	res.redirect(301, redirect+url.substring(1));
 })
 module.exports = app;
